7cf2de40b7e9d441a06855007d82bf103ae554c4
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-05-31 18:20:09 +0530

    More Period non-io tests to seperate file

---
+++ tests/types/time/period.cpp
@@ -0,0 +1,49 @@
+#include "../../catch.hpp"
+#include "../../common/time_utils.hpp"
+#include "../../common/utils.hpp"
+#include <meos/io/Deserializer.hpp>
+#include <meos/io/Serializer.hpp>
+#include <meos/types/time/Period.hpp>
+
+TEST_CASE("Period overlap", "[period]") {
+  SECTION("clear overlap") {
+    auto left_open = GENERATE(true, false);
+    auto right_open = GENERATE(true, false);
+    auto period_1 =
+        *make_unique<Period>(unix_time(2012, 1, 1), unix_time(2012, 4, 1),
+                             left_open, right_open)
+             .get();
+    auto period_2 =
+        *make_unique<Period>(unix_time(2012, 2, 1), unix_time(2012, 5, 1),
+                             left_open, right_open)
+             .get();
+    REQUIRE(period_1.overlap(period_2));
+  }
+  SECTION("clearly no overlap") {
+    auto left_open = GENERATE(true, false);
+    auto right_open = GENERATE(true, false);
+    auto period_1 =
+        *make_unique<Period>(unix_time(2012, 1, 1), unix_time(2012, 2, 1),
+                             left_open, right_open)
+             .get();
+    auto period_2 =
+        *make_unique<Period>(unix_time(2012, 4, 1), unix_time(2012, 5, 1),
+                             left_open, right_open)
+             .get();
+    REQUIRE(!period_1.overlap(period_2));
+  }
+  SECTION("borderline overlap case when only both periods are inclusive") {
+    auto left_open = GENERATE(true, false);
+    auto right_open = GENERATE(true, false);
+    auto period_1 =
+        *make_unique<Period>(unix_time(2012, 1, 1), unix_time(2012, 3, 1),
+                             left_open, right_open)
+             .get();
+    auto period_2 =
+        *make_unique<Period>(unix_time(2012, 3, 1), unix_time(2012, 5, 1),
+                             left_open, right_open)
+             .get();
+    bool expected = !period_1.isRightOpen() && !period_2.isLeftOpen();
+    REQUIRE(period_1.overlap(period_2) == expected);
+  }
+}
