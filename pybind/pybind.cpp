4700412eb221ca958adbe3d0568548726b23367f
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-06-25 11:38:25 +0530

    [Period] Expose time_t as datetime to python

--- pybind/pybind.cpp
+++ pybind/pybind.cpp
@@ -2,13 +2,42 @@
 #include <meos/io/Serializer.hpp>
 #include <meos/types/temporal/TInstant.hpp>
 #include <meos/types/temporal/TInstantSet.hpp>
+#include <pybind11/chrono.h>
+#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 extern "C" {
 #include <meos/geos.h>
 }
 
+using namespace pybind11::literals;
 namespace py = pybind11;
+
+py::object time_t_to_datetime(time_t tt) {
+  py::module datetime_module = py::module::import("datetime");
+
+  auto tzinfo = datetime_module.attr("timezone").attr("utc");
+  return datetime_module.attr("datetime")
+      .attr("fromtimestamp")(tt / 1000, "tz"_a = tzinfo);
+}
+
+time_t datetime_to_time_t(py::object dt) {
+  double seconds = dt.attr("timestamp")().cast<py::float_>();
+  return seconds * 1000;
+}
+
+py::object time_t_to_timedelta(time_t tt) {
+  py::module datetime_module = py::module::import("datetime");
+
+  auto tzinfo = datetime_module.attr("timezone").attr("utc");
+  return datetime_module.attr("timedelta")("milliseconds"_a = tt);
+}
+
+time_t timedelta_to_time_t(py::object delta_ms) {
+  py::object total_seconds_object = delta_ms.attr("total_seconds")();
+  float total_seconds = total_seconds_object.cast<py::float_>();
+  return total_seconds * 1000;
+}
 
 template <typename T>
 void declare_temporal_types(py::module &m, std::string const &typesuffix) {
@@ -76,29 +105,26 @@ PYBIND11_MODULE(pymeos, m) {
   declare_temporal_types<Geometry>(m, "Geom");
 
   py::class_<Period>(m, "Period")
-      .def(py::init<time_t const, time_t const, bool const, bool const>())
-      .def(py::init<string const, string const, bool const, bool const>())
-      .def(py::init<string const>())
-      .def(
-          "__eq__",
-          [](Period const &self, Period const &other) { return self == other; },
-          py::is_operator())
-      .def(
-          "__lt__",
-          [](Period const &self, Period const &other) { return self < other; },
-          py::is_operator())
-      .def(
-          "__le__",
-          [](Period const &self, Period const &other) { return self <= other; },
-          py::is_operator())
-      .def(
-          "__gt__",
-          [](Period const &self, Period const &other) { return self > other; },
-          py::is_operator())
-      .def(
-          "__ge__",
-          [](Period const &self, Period const &other) { return self >= other; },
-          py::is_operator())
+      .def(py::init<time_t const, time_t const, bool const, bool const>(),
+           py::arg("lower"), py::arg("upper"), py::arg("lower_inc") = true,
+           py::arg("upper_inc") = false)
+      .def(py::init<string const, string const, bool const, bool const>(),
+           py::arg("lower"), py::arg("upper"), py::arg("lower_inc") = true,
+           py::arg("upper_inc") = false)
+      .def(py::init<string const>(), py::arg("serialized"))
+      .def(py::init([](py::object lower_dt, py::object upper_dt,
+                       bool const lower_inc, bool const upper_inc) {
+             time_t lower = datetime_to_time_t(lower_dt);
+             time_t upper = datetime_to_time_t(upper_dt);
+             return Period(lower, upper, lower_inc, upper_inc);
+           }),
+           py::arg("lower"), py::arg("upper"), py::arg("lower_inc") = true,
+           py::arg("upper_inc") = false)
+      .def(py::self == py::self)
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
       .def("__hash__",
            [](Period const &period) {
              return py::hash(py::make_tuple(period.lower(), period.upper(),
@@ -111,14 +137,28 @@ PYBIND11_MODULE(pymeos, m) {
              s << self;
              return s.str();
            })
-      .def_property_readonly("lower", &Period::lower)
-      .def_property_readonly("upper", &Period::upper)
+      .def_property_readonly("lower",
+                             [](Period const &period) {
+                               return time_t_to_datetime(period.lower());
+                             })
+      .def_property_readonly("upper",
+                             [](Period const &period) {
+                               return time_t_to_datetime(period.upper());
+                             })
       .def_property_readonly("lower_inc", &Period::lower_inc)
       .def_property_readonly("upper_inc", &Period::upper_inc)
-      .def_property_readonly("timespan", &Period::timespan)
-      .def("shift", &Period::shift)
+      .def_property_readonly("timespan",
+                             [](Period const &period) {
+                               return time_t_to_timedelta(period.timespan());
+                             })
+      .def("shift",
+           [](Period const &period, py::object delta_ms) {
+             return period.shift(timedelta_to_time_t(delta_ms));
+           })
       .def("overlap", &Period::overlap)
-      .def("contains_timestamp", &Period::contains_timestamp);
+      .def("contains_timestamp", [](Period const &period, py::object dt) {
+        return period.contains_timestamp(datetime_to_time_t(dt));
+      });
 
   py::class_<PeriodSet>(m, "PeriodSet")
       .def(py::init<set<Period> &>())
