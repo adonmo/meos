c213bfee6dbe53984a2cfe3965395c657c35f646
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-06-28 16:34:56 +0530

    [Temporal Types] Expose all existing functions to python

--- pybind/include/temporal_module.hpp
+++ pybind/include/temporal_module.hpp
@@ -46,12 +46,12 @@ void declare_temporal_types(py::module &m, std::string const &typesuffix) {
       .def(py::init<string, string>(), py::arg("value"), py::arg("timestamp"))
       .def(py::init<pair<string, string>>(), py::arg("instant"))
       .def(py::init<string>(), py::arg("serialized"))
-      .def(py::self == py::self)
-      .def(py::self != py::self)
-      .def(py::self < py::self)
-      .def(py::self <= py::self)
-      .def(py::self > py::self)
-      .def(py::self >= py::self)
+      .def(py::self == py::self, py::arg("other"))
+      .def(py::self != py::self, py::arg("other"))
+      .def(py::self < py::self, py::arg("other"))
+      .def(py::self <= py::self, py::arg("other"))
+      .def(py::self > py::self, py::arg("other"))
+      .def(py::self >= py::self, py::arg("other"))
       .def("__str__",
            [](TInstant<T> const &self) {
              std::ostringstream s;
@@ -64,7 +64,19 @@ void declare_temporal_types(py::module &m, std::string const &typesuffix) {
                  py::make_tuple(instant.getValue(), instant.getTimestamp()));
            })
       .def("getTimestamp", &TInstant<T>::getTimestamp)
-      .def("getValue", &TInstant<T>::getValue);
+      .def("getValue", &TInstant<T>::getValue)
+      .def("compare", &TInstant<T>::compare)
+      .def("duration", &TInstant<T>::duration)
+      .def("instants", &TInstant<T>::instants)
+      .def("getValues", &TInstant<T>::getValues)
+      .def("timestamps", &TInstant<T>::timestamps)
+      .def("getTime", &TInstant<T>::getTime)
+      .def("period", &TInstant<T>::period)
+      .def("shift", &TInstant<T>::shift, py::arg("timedelta"))
+      .def("intersectsTimestamp", &TInstant<T>::intersectsTimestamp,
+           py::arg("datetime"))
+      .def("intersectsPeriod", &TInstant<T>::intersectsPeriod,
+           py::arg("period"));
 
   def_comparator<TemporalComparators<TInstantSet<T>>>(m, "TInstantSet",
                                                       typesuffix);
@@ -73,20 +85,32 @@ void declare_temporal_types(py::module &m, std::string const &typesuffix) {
   py::class_<TInstantSet<T>, TemporalComparators<TInstantSet<T>>,
              TInstantFunctions<TInstantSet<T>, TInstant<T>, T>>(
       m, ("TInstantSet" + typesuffix).c_str())
-      .def(py::init<set<TInstant<T>> &>())
-      .def(py::self == py::self)
-      .def(py::self != py::self)
-      .def(py::self < py::self)
-      .def(py::self <= py::self)
-      .def(py::self > py::self)
-      .def(py::self >= py::self)
+      .def(py::init<set<TInstant<T>> &>(), py::arg("instants"))
+      .def(py::self == py::self, py::arg("other"))
+      .def(py::self != py::self, py::arg("other"))
+      .def(py::self < py::self, py::arg("other"))
+      .def(py::self <= py::self, py::arg("other"))
+      .def(py::self > py::self, py::arg("other"))
+      .def(py::self >= py::self, py::arg("other"))
       .def("__str__",
            [](TInstantSet<T> const &self) {
              std::ostringstream s;
              s << self;
              return s.str();
            })
-      .def("getInstants", &TInstantSet<T>::getInstants);
+      .def("getInstants", &TInstantSet<T>::getInstants)
+      .def("compare", &TInstantSet<T>::compare)
+      .def("duration", &TInstantSet<T>::duration)
+      .def("instants", &TInstantSet<T>::instants)
+      .def("getValues", &TInstantSet<T>::getValues)
+      .def("timestamps", &TInstantSet<T>::timestamps)
+      .def("getTime", &TInstantSet<T>::getTime)
+      .def("period", &TInstantSet<T>::period)
+      .def("shift", &TInstantSet<T>::shift, py::arg("timedelta"))
+      .def("intersectsTimestamp", &TInstantSet<T>::intersectsTimestamp,
+           py::arg("datetime"))
+      .def("intersectsPeriod", &TInstantSet<T>::intersectsPeriod,
+           py::arg("period"));
 
   def_comparator<TemporalComparators<TSequence<T>>>(m, "TSequence", typesuffix);
   def_tinstant_functions<TInstantFunctions<TSequence<T>, TInstant<T>, T>>(
@@ -94,13 +118,14 @@ void declare_temporal_types(py::module &m, std::string const &typesuffix) {
   py::class_<TSequence<T>, TemporalComparators<TSequence<T>>,
              TInstantFunctions<TSequence<T>, TInstant<T>, T>>(
       m, ("TSequence" + typesuffix).c_str())
-      .def(py::init<vector<TInstant<T>> &, bool, bool>())
-      .def(py::self == py::self)
-      .def(py::self != py::self)
-      .def(py::self < py::self)
-      .def(py::self <= py::self)
-      .def(py::self > py::self)
-      .def(py::self >= py::self)
+      .def(py::init<vector<TInstant<T>> &, bool, bool>(), py::arg("instants"),
+           py::arg("lower_inc") = true, py::arg("upper_inc") = false)
+      .def(py::self == py::self, py::arg("other"))
+      .def(py::self != py::self, py::arg("other"))
+      .def(py::self < py::self, py::arg("other"))
+      .def(py::self <= py::self, py::arg("other"))
+      .def(py::self > py::self, py::arg("other"))
+      .def(py::self >= py::self, py::arg("other"))
       .def("__str__",
            [](TSequence<T> const &self) {
              std::ostringstream s;
@@ -109,7 +134,19 @@ void declare_temporal_types(py::module &m, std::string const &typesuffix) {
            })
       .def_readonly("lower_inc", &TSequence<T>::lower_inc)
       .def_readonly("upper_inc", &TSequence<T>::upper_inc)
-      .def("getInstants", &TSequence<T>::getInstants);
+      .def("getInstants", &TSequence<T>::getInstants)
+      .def("compare", &TSequence<T>::compare)
+      .def("duration", &TSequence<T>::duration)
+      .def("instants", &TSequence<T>::instants)
+      .def("getValues", &TSequence<T>::getValues)
+      .def("timestamps", &TSequence<T>::timestamps)
+      .def("getTime", &TSequence<T>::getTime)
+      .def("period", &TSequence<T>::period)
+      .def("shift", &TSequence<T>::shift, py::arg("timedelta"))
+      .def("intersectsTimestamp", &TSequence<T>::intersectsTimestamp,
+           py::arg("datetime"))
+      .def("intersectsPeriod", &TSequence<T>::intersectsPeriod,
+           py::arg("period"));
 }
 
 void def_temporal_module(py::module &m) {
