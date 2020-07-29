b598e181114d35d884914bc11c13551013aab47d
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-07-29 17:44:48 +0530

    Clone GEOS 3.8 instead of master for stable builds

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -6,8 +6,8 @@
 # Get latest cmake
 pip install cmake
 
-# Clone, configure, build and install GEOS
-git clone https://git.osgeo.org/gitea/geos/geos.git --depth=1
+# Clone, configure, build and install GEOS 3.8
+git clone https://github.com/libgeos/geos.git --branch 3.8 --depth=1
 cmake -B build -S geos -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 cmake --install build --config Release
