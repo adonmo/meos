e971e11062614ab324dcd7c2aec6684e6534994f
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-09-15 12:40:19 +0530

    C API: Expose TFloatSeqSet's atPeriod function

--- capi/include/meos/meos_c.h
+++ capi/include/meos/meos_c.h
@@ -1,17 +1,38 @@
 #pragma once
 
+#include <string.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct TBox TBox;
+typedef struct Period Period;
+typedef struct TFloatSeqSet TFloatSeqSet;
+
+// TBox
 
 TBox *newTBox(double const xmin, double const xmax);
 
 double TBox_xmin(TBox *tbox);
 double TBox_xmax(TBox *tbox);
 
-void deleteTBox(TBox *v);
+void deleteTBox(TBox *tbox);
+
+// Period
+
+Period *newPeriod(char *serialized);
+
+void deletePeriod(Period *period);
+
+// TFloatSeqSet
+
+TFloatSeqSet *newTFloatSeqSet(char *serialized);
+
+TFloatSeqSet *TFloatSeqSet_atPeriod(TFloatSeqSet *tfloatseqset, Period *period);
+unsigned char *TFloatSeqSet_str(TFloatSeqSet *tfloatseqset, size_t *size);
+
+void deleteTFloatSeqSet(TFloatSeqSet *tfloatseqset);
 
 #ifdef __cplusplus
 }
--- capi/source/meos_c.cpp
+++ capi/source/meos_c.cpp
@@ -1,8 +1,15 @@
 #include <meos/meos_c.h>
 
+#include <cstring>
 #include <meos/types/box/TBox.hpp>
+#include <meos/types/temporal/TSequenceSet.hpp>
+#include <meos/types/time/Period.hpp>
+#include <sstream>
 
 extern "C" {
+
+// TBox
+
 TBox *newTBox(double const xmin, double const xmax) {
   return reinterpret_cast<TBox *>(new meos::TBox(xmin, xmax));
 }
@@ -19,6 +26,50 @@ double TBox_xmax(TBox *tbox) {
 
 void deleteTBox(TBox *tbox) {
   auto t = reinterpret_cast<meos::TBox *>(tbox);
+  delete t;
+}
+
+// Period
+
+Period *newPeriod(char *serialized) {
+  return reinterpret_cast<Period *>(new meos::Period(serialized));
+}
+
+void deletePeriod(Period *period) {
+  auto t = reinterpret_cast<meos::Period *>(period);
+  delete t;
+}
+
+// TFloatSeqSet
+
+TFloatSeqSet *newTFloatSeqSet(char *serialized) {
+  return reinterpret_cast<TFloatSeqSet *>(new meos::TFloatSeqSet(serialized));
+}
+
+TFloatSeqSet *TFloatSeqSet_atPeriod(TFloatSeqSet *tfloatseqset, Period *period) {
+  auto t = reinterpret_cast<meos::TFloatSeqSet *>(tfloatseqset);
+  auto p = reinterpret_cast<meos::Period *>(period);
+  auto r = t->atPeriod(*p);
+  return reinterpret_cast<TFloatSeqSet *>(new meos::TFloatSeqSet(r));
+}
+
+// Remember to free the result!
+unsigned char *TFloatSeqSet_str(TFloatSeqSet *tfloatseqset, size_t *size) {
+  auto t = reinterpret_cast<meos::TFloatSeqSet *>(tfloatseqset);
+  std::stringstream output;
+  output << *t;
+  auto s = output.str();
+  const std::size_t len = s.length();
+  unsigned char *result = static_cast<unsigned char *>(malloc(len));
+  if (result) {
+    std::memcpy(result, s.c_str(), len);
+    *size = len;
+  }
+  return result;
+}
+
+void deleteTFloatSeqSet(TFloatSeqSet *tfloatseqset) {
+  auto t = reinterpret_cast<meos::TFloatSeqSet *>(tfloatseqset);
   delete t;
 }
 }
